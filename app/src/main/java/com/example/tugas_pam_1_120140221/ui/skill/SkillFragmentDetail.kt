b4327751d0cd2083c67214d00833a2fecc45e779
krisnasaputtra <krisna.saputra031002@gmail.com>
2023-09-25 21:35:59 +0700

    First Commit

---
+++ app/src/main/java/com/example/tugas_pam_1_120140221/ui/skill/SkillFragmentDetail.kt
@@ -0,0 +1,4 @@
+package com.example.tugas_pam_1_120140221.ui.skill
+
+class SkillFragmentDetail {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Tugas_PAM_1_120140221"
+include(":app")
